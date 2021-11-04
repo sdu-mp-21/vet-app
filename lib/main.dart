fc5e4e0db5250f9bd241239c4456a94d3368b817
Ulan  Abikhan <u.abikhan02@mail.ru>
2021-11-04 14:26:00 +0600

    change

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'main_screen.dart';
 import 'login_page.dart';
 
 
+
 void main() => runApp(MyApp());
+
 class StaticApp extends StatelessWidget {
  @override
  Widget build(BuildContext context) {
