e9bd753719008b5f8b2ebbf7e61ef597cb85433a
alibek2001 <86375876+alibek2001@users.noreply.github.com>
2021-12-02 14:06:34 +0600

    commit from Alibek(Owner Page)

---
+++ lib/utils/animal_count_data.dart
@@ -0,0 +1,42 @@
+const animalCountData = [
+  {
+    "animal_type": 'КРС',
+    "count": 22,
+  },
+  {
+    "animal_type": 'МРС',
+    "count": 5,
+  },
+  {
+    "animal_type": 'Свинья',
+    "count": 5,
+  },
+  {
+    "animal_type": 'Верблюд',
+    "count": 1,
+  },
+  {
+    "animal_type": 'Лошадь',
+    "count": 9,
+  },
+  {
+    "animal_type": 'КРС',
+    "count": 22,
+  },
+  {
+    "animal_type": 'МРС',
+    "count": 5,
+  },
+  {
+    "animal_type": 'Свинья',
+    "count": 5,
+  },
+  {
+    "animal_type": 'Верблюд',
+    "count": 1,
+  },
+  {
+    "animal_type": 'Лошадь',
+    "count": 9,
+  },
+];
