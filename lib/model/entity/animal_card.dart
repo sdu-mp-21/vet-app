6999534a0471650dace7b2fced48dc16d39df50c
ulan-abikhan <70633944+ulan-abikhan@users.noreply.github.com>
2021-12-09 14:46:56 +0600

    model updated

--- lib/model/entity/animal_card.dart
+++ lib/model/entity/animal_card.dart
@@ -1,14 +1,26 @@
 import 'package:json_annotation/json_annotation.dart';
 
+part 'animal_card.g.dart';
+
 @JsonSerializable(explicitToJson: true)
 class OneAnimalCard {
   final String ing;
   final String imgUrl;
   final String gender;
+  final String date;
+  final String breed;
+  final String nj;
 
   OneAnimalCard({
     required this.ing,
     required this.imgUrl,
-    required this.gender
+    required this.gender,
+    required this.date,
+    required this.breed,
+    required this.nj
   });
+
+  factory OneAnimalCard.fromJson(Map<String, dynamic> data) => _$OneAnimalFromJson(data);
+  Map<String, dynamic> toJson() => _$OneAnimalToJson(this);
+
 }
